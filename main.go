d9123cea6e1c737ce1f49bab1f9df5360c07beaa
Martinez, Jhun Rhenz <jhunmartinez1@gmail.com>
2021-12-09 21:05:32 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,40 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"building-go-microservices/handlers"
 )
 
 func main() {
 
-	// handle func is a convenience method, registers a function a path to a default serve muks
-	// default serve mux is an http handler, everything served in Go is an http handler.
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World") // handle func on a path and executes hello world
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Oops", http.StatusBadRequest)
-			// rw.WriteHeader(http.StatusBadRequest)
-			// rw.Write([]byte("Ooops"))
-			return
-		}
+	// logger
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	// create reference to the handler
+	hh := handlers.NewHello(l)
 
-		// log.Printf("Data %s\n", d)
-		// how to write back to user? by using ResponseWriter
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
+	// create a new ServeMux now register handler hh to a server
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye World") // handle func on a path and executes hello world
-	})
-
-	http.ListenAndServe(":9090", nil) // server, nil uses default serve mux
-
-	// https://go.dev/src/net/http/server.go?s=61509%3A61556#L2378
-
-	// current program is only logging not really useful
-	// how do we read and write to a request? By using ResponseWriter and http.Request
+	http.ListenAndServe(":9090", sm) // server, nil uses default serve mux
 
 }
