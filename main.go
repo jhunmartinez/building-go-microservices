d793ef0fe5520281fa020cfa774ee5afb3e2947c
Martinez, Jhun Rhenz <jhunmartinez1@gmail.com>
2021-12-16 18:58:21 -0500

    refactoring with gorilla framework
    deleted servehttp. Added middleware validation

--- main.go
+++ main.go
@@ -21,11 +21,19 @@ func main() {
 	// gh := handlers.NewGoodbye(l)
 	ph := handlers.NewProducts(l)
 
-	// create a new ServeMux now register handler hh to a server
-	sm := http.NewServeMux()
-	// sm.Handle("/", hh)
-	// sm.Handle("/goodbye", gh)
-	sm.Handle("/", ph)
+	// create a new serve mux and register the handlers
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareValidateProduct)
 
 	// in depth server, create a new server
 	s := &http.Server{
