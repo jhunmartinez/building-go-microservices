f0e11194c144fe1cf0a2d1b87ee375b0c17376b3
Martinez, Jhun Rhenz <jhunmartinez1@gmail.com>
2021-12-15 20:27:13 -0500

    updated products.go for handler and data. Implemented REST services
    GET, POST, PUT functionality added in the products. go files

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -25,13 +25,46 @@ type Product struct {
 type Products []*Product
 
 // can add encapsulation for JSON to these handy methods
+// https://golang.org/pkg/encoding/json/#NewEncoder
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
 }
 
+// GetProducts returns a list of products
 func GetProducts() Products {
 	return productList
+}
+
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
+}
+
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
 }
 
 var productList = []*Product{
